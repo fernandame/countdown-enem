8273dde78b699a9bdc028d37e543ae97db94dfa2
fernanda <fernandamexquita@gmail.com>
2024-04-26 19:09:27 -0300

    removing hours,  minutes and seconds for now

--- src/CountdownTimer.js
+++ src/CountdownTimer.js
@@ -9,21 +9,21 @@ const CountdownTimer = ({ targetDate }) => {
       <ShowCounter
         weeks={weeks}
         days={days}
-        hours={hours}
-        minutes={minutes}
-        seconds={seconds}
+        // hours={hours}
+        // minutes={minutes}
+        // seconds={seconds}
       />
     );
 };
 
 const ShowCounter = ({ weeks, days, hours, minutes, seconds }) => {
     return (
-      <div className="flex flex-col h-screen w-full justify-evenly">
+      <div className="flex flex-col h-screen w-full justify-center gap-10">
           <DateTimeDisplay value={weeks} type={'Semanas'} />
           <DateTimeDisplay value={days} type={'Dias'} />
-          <DateTimeDisplay value={hours} type={'Horas'} />
+          {/* <DateTimeDisplay value={hours} type={'Horas'} />
           <DateTimeDisplay value={minutes} type={'Minutos'} />
-          <DateTimeDisplay value={seconds} type={'Segundos'}/>
+          <DateTimeDisplay value={seconds} type={'Segundos'}/> */}
       </div>
     );
   };
