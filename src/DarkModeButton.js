dd66ff259c45a1326e072678a1c2728a37831d05
fernanda <fernandamexquita@gmail.com>
2024-04-27 02:57:15 -0300

    details

--- src/DarkModeButton.js
+++ src/DarkModeButton.js
@@ -5,7 +5,7 @@ const cx = require('classnames');
 
 const DarkModeButton = ({onClick, darkMode}) => {
   return (
-    <button className={cx('w-8 h-8 rounded-full flex items-center justify-center cursor-pointer', darkMode ? "hover:bg-white/10" : "hover:bg-gray-200")} onClick={onClick}>
+    <button className={cx('w-8 h-8 rounded-full flex items-center justify-center cursor-pointer -mb-4', darkMode ? "hover:bg-white/10" : "hover:bg-gray-200")} onClick={onClick}>
         {darkMode
             ? <img src={Sun} alt="Modo Claro" className='p-0.5'/>
             : <img src={Moon} alt="Modo Noturno" />
