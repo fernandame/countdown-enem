7a589df19bb285464afd7fe0f149e5a75fedcab2
fernanda <fernandamexquita@gmail.com>
2024-04-27 01:36:07 -0300

    remove transition to make it smooth

--- src/DarkModeButton.js
+++ src/DarkModeButton.js
@@ -5,7 +5,7 @@ const cx = require('classnames');
 
 const DarkModeButton = ({onClick, darkMode}) => {
   return (
-    <button className={cx('absolute left-2 top-2 w-8 h-8 rounded-full flex items-center justify-center cursor-pointer transition', darkMode ? "hover:bg-white/10" : "hover:bg-gray-200")} onClick={onClick}>
+    <button className={cx('absolute left-2 top-2 w-8 h-8 rounded-full flex items-center justify-center cursor-pointer', darkMode ? "hover:bg-white/10" : "hover:bg-gray-200")} onClick={onClick}>
         {darkMode
             ? <img src={Sun} alt="Modo Claro" className='p-0.5'/>
             : <img src={Moon} alt="Modo Noturno" />
